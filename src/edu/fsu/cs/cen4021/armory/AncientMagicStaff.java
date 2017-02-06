d1111e695b086393e839330ebc62a32a90074cf3
Alexander Sumner <alexcsumner@gmail.com>
2017-02-06 15:42:49 -0500

    implemented AncientMagicStaff class

--- src/edu/fsu/cs/cen4021/armory/AncientMagicStaff.java
+++ src/edu/fsu/cs/cen4021/armory/AncientMagicStaff.java
@@ -1,7 +1,71 @@
 package edu.fsu.cs.cen4021.armory;
 
+import java.io.*;
+import java.util.Collections;
+import java.util.List;
+
 /**
  * Created by Sumner on 2/6/2017.
  */
-public class AncientMagicStaff {
+public class AncientMagicStaff implements Weapon {
+
+    int DAMAGE;
+
+    AncientMagicStaff() {
+        String filename = "ancientstaff.obj";
+        List<Integer> calc;
+        int mult;
+
+        try {
+            FileInputStream fis = new FileInputStream(filename);
+            ObjectInputStream ois = new ObjectInputStream(fis);
+
+            calc = (List<Integer>) ois.readObject();
+
+            for(int i = 0; i < calc.size(); i++) {
+               mult = calc.get(i) * calc.get(i);
+               calc.add(i,mult);
+            }
+            calc.remove(1);
+            calc.remove(4);
+
+            Collections.reverse(calc);
+
+            DAMAGE = calc.get(0) + calc.get(2) + calc.get(6);
+
+        }
+        catch (FileNotFoundException ex) {
+            System.out.println(
+                    "Unable to open file '" +
+                            filename + "'");
+        }
+        catch (IOException ex) {
+            System.out.println(
+                    "Error reading file '"
+                            + filename + "'");
+            // Or we could just do this:
+            // ex.printStackTrace();
+        }
+        catch (ClassNotFoundException ex) {
+            System.err.println("A ClassNotFoundException was caught: " + ex.getMessage());
+            ex.printStackTrace();
+        }
+    }
+
+    @Override
+    public int hit() {
+        return DAMAGE;
+    }
+
+    @Override
+    public int hit(int armor) {
+        int less = armor / 4;
+        armor = armor - less;
+
+        int damage = DAMAGE - armor;
+        if (damage < 0) {
+            return 0;
+        }
+        return damage;
+    }
 }
